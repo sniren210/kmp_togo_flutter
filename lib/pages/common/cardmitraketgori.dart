206d1ad15bd9087298d740a66d3efb44fd6b26bc
INKY PRAMUDIA RAMDHANI <107750059+ingkay27@users.noreply.github.com>
2023-02-13 00:02:30 +0700

    feat: Gradient Color in cardMitraKategori

--- lib/pages/common/cardmitraketgori.dart
+++ lib/pages/common/cardmitraketgori.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
-cardmitraketegori(String? judul, Color? color) {
-  return Container(
-    width: 28.w,
-    margin: EdgeInsets.only(
-      right: 3.w,
-    ),
-    decoration:
-        BoxDecoration(borderRadius: BorderRadius.circular(5), color: color),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          judul ?? "",
-          style: TextStyle(color: Colors.white, fontSize: 9.sp),
-        ),
-      ],
-    ),
-  );
+class cardMitraKategori extends StatelessWidget {
+  const cardMitraKategori({
+    super.key,
+    required this.title,
+    required this.color,
+  });
+
+  final String title;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 28.w,
+      margin: EdgeInsets.only(
+        right: 3.w,
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black,
+              color,
+            ],
+          )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 9.sp),
+          ),
+        ],
+      ),
+    );
+  }
 }
