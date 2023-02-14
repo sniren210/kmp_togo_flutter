623e55db6b23648303ef065f677843d205668bb3
INKY PRAMUDIA RAMDHANI <107750059+ingkay27@users.noreply.github.com>
2023-02-14 19:28:20 +0700

    feat: API on detail produk

--- lib/pages/profile/profile_detail.dart
+++ lib/pages/profile/profile_detail.dart
@@ -5,6 +5,12 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kmp_togo_mobile/apis/repository.dart';
+import 'package:kmp_togo_mobile/pages/base.dart';
+import 'package:kmp_togo_mobile/providers/cart/cart_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/account/provider_account.dart';
 
 class profileDetail extends StatelessWidget {
   const profileDetail({super.key});
@@ -27,140 +33,150 @@ class profileDetail extends StatelessWidget {
         //   centerTitle: true,
         //   backgroundColor: Theme.of(context).primaryColor,
         // ),
-        body: Container(
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(120),
-                        bottomRight: Radius.circular(120),
+        body: BaseWidget<ProviderAccountInfo>(
+            model: ProviderAccountInfo(Repository()),
+            onModelReady: (model) => model.fetchAccountInfo(),
+            child: Container(),
+            builder: (context, model, child) {
+              return Consumer<CartProvider>(
+                  builder: (BuildContext context, provider, widget) {
+                return Container(
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              borderRadius: const BorderRadius.only(
+                                bottomLeft: Radius.circular(120),
+                                bottomRight: Radius.circular(120),
+                              ),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children:  [
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  model.items!.user.currentMember.name,
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  model.items!.user.email,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 70),
+                              child: Container(
+                                width: 80,
+                                height: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: Theme.of(context).primaryColor,
+                                  border: Border.all(
+                                    color: Colors.white,
+                                    width: 2,
+                                  ),
+                                ),
+                                child: Icon(
+                                  Icons.account_circle,
+                                  size: 60,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          'Inky Pramudia Ramdhani',
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'id: 97867674354',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 70),
-                      child: Container(
-                        width: 80,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: Theme.of(context).primaryColor,
-                          border: Border.all(
-                            color: Colors.white,
-                            width: 2,
-                          ),
-                        ),
-                        child: Icon(
-                          Icons.account_circle,
-                          size: 60,
-                          color: Colors.white,
-                        ),
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      children: [
-                        const ListTile(
-                          title: Text('Gender'),
-                          subtitle: Text('laki"'),
-                        ),
-                        ListTile(
-                          title: const Text('Tempat, Tanggal lahir'),
-                          subtitle: Row(
-                            children: const [
-                              Text('Majalengka'),
-                              Text('20-11-2003'),
-                            ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text('Gender'),
+                                  subtitle: Text(model.items!.user.name),
+                                ),
+                                ListTile(
+                                  title: const Text('Tempat, Tanggal lahir'),
+                                  subtitle: Row(
+                                    children: [
+                                      Text(model.items!.user.currentMember.birthPlace.toString()),
+                                      Text(model.items!.user.currentMember.birthDate.toString()),
+                                    ],
+                                  ),
+                                ),
+                                ListTile(
+                                  title: const Text('Agama'),
+                                  subtitle: Text(model.items!.user.currentMember.name),
+                                ),
+                              ],
+                            ),
                           ),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text('Status perkawinan'),
+                                  subtitle: Text(model.items!.user.currentMember.name),
+                                ),
+                                ListTile(
+                                    title: Text('pekerjaan'),
+                                    subtitle: Text(model.items!.user.currentMember.name)),
+                                ListTile(
+                                  title: Text('Kewarganegaraan'),
+                                  subtitle: Text(model.items!.user.currentMember.name),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      ListTile(
+                        title: Text('alamat'),
+                        subtitle: Text(
+                            model.items!.user.currentMember.address.toString()),
+                      ),
+                      ListTile(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(model.items!.user.currentMember.otherBank.toString()),
+                            Text(model.items!.user.currentMember.name),
+                          ],
                         ),
-                        const ListTile(
-                          title: Text('Agama'),
-                          subtitle: Text('ISLAM"'),
+                        subtitle: Text(nomorRekening.toString()),
+                        trailing: InkWell(
+                          onTap: () {
+                            _showSimpleModalDialog(context, nomorRekening);
+                          },
+                          child: const Icon(Icons.edit),
                         ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                  Expanded(
-                    child: Column(
-                      children: const [
-                        ListTile(
-                          title: Text('Status perkawinan'),
-                          subtitle: Text('jomblo'),
-                        ),
-                        ListTile(
-                            title: Text('pekerjaan'), subtitle: Text('PNS')),
-                        ListTile(
-                          title: Text('Kewarganegaraan'),
-                          subtitle: Text('WNI'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const ListTile(
-                title: Text('alamat'),
-                subtitle: Text(
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
-              ),
-              ListTile(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Artha Graha Internasional'),
-                    Text('INKY PRAMUDIA RAMDHANI'),
-                  ],
-                ),
-                subtitle: Text(nomorRekening.toString()),
-                trailing: InkWell(
-                  onTap: () {
-                    _showSimpleModalDialog(context, nomorRekening);
-                  },
-                  child: const Icon(Icons.edit),
-                ),
-              )
-            ],
-          ),
-        ),
+                );
+              });
+            }),
       ),
     );
   }
